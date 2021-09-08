3cc0a069359843381f74031004155f83f581ed13
adipurnama83 <adi.purnama1983@gmail.com>
2021-09-08 13:59:50 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,7 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Linux; Android 10; Infinix X692) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.114 Mobile Safari/537.36";
+
+$cookie = "_ga=GA1.1.1807481357.1630961880;a=ZUM35f4vlUi2Fk9zsW0kbx2GLOOt0NNF;token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAYTcjGAFhNyMZgAGBAsAAIHIAzEQ8aN5WW00IPtZlr8b2USjSWWwqANsw5PkkPON9wQBHMEUCIAT7CGXeJzGEaWnGeGb2o7aAOHmPkewAs8zWToS2jZ-vAiEA5wQBNthaCk0iE_YPmiKfdWhaPpwDsAQMd60rk5j-vN0;__cf_bm=Lj5Dyxk4wLCrfKTiH0r5.kCEJdxeNdswnJ2bOxdpZrQ-1631015244-0-Aad15gLrFw5xFlklJo1uWWo8OSr5O1N6XJBt1zFwK0IDWeRzTmAghQG1RVdMt9EGfCPGMKB5bMs0TvvKSviLhcTXRJZLvpfCuNhdPMugRCKEfxgdIoUctXOFL/wySrZs7Q==;PHPSESSID=9d1a33bcd5e7e125afe3eb396894bb40;_ga_97QF2Q8DFH=GS1.1.1631015238.5.1.1631015249.0";
+
+$urlclaim = "https://faucet77.tk/faucet.php?address=DDYXLCXMzDFyETLVaYNSawoocXryesxLuv&currency=DOGE&key=b738d09c71bb770043f48a785a875b0c&__cf_chl_managed_tk__=pmd_ycTeoxgG58c7NG5RAIIQ0PQbJUQ3aV0zibeIx9GHJgo-1631084166-0-gqNtZGzNA3ujcnBszQhR";
